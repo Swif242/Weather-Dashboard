e7e4f5341650fab988f97694e5c9f28ed4e9e9b1
Jeremy N <no-reply>
2020-03-18 16:35:01 -0700

    added some css and comments, able to store recent search but not display after refresh

--- script.js
+++ script.js
@@ -32,37 +32,46 @@ $(dateFive).text("Date: " + moment().add(5, "day").format("MMM-DD-YYYY"));
 $(today).text("Today is: " + currentDate);
 
 
-
-
+// day forecast info template
 var foreCast = [
     {
         temp: "Temp: ",
         humidity: "Humidity: ",
-        wind: "Wind Speed: ",
+        wind: "Wind: ",
         index: "UV Index: ",
         cloud: "Clouds: ",
     },
 
 
 ]
-
+//  submit button event listener
 $(submit).on("click", function (event) {
     event.preventDefault();
     var input = $.trim($("#inputbox").val());
+    localStorage.setItem("mycity", input);
+    var recentSearch = localStorage.getItem("mycity");
+
+    // takes the value from the input box and writes to the city variable
+    $(city).text("City: " + input);
+    $("#recent").append("<li>" + recentSearch + "</li>");
+
 
     if (input == "") {
         alert("Must enter a city to continue");
 
     };
 
-    //   takes the value from the input box and writes to the city variable
-    $(city).text("City: " + input);
-    $("#recent").append("<li>" + input + "</li>");
+
+
+    
+
+
+
 
     //   clear input box after submitted (doesn't work yet)
-    $(input).remove();
+    $(input).text("");
 
-
+    //  api key and api web adress with units set to imperial for correct temp reading
     var key = "9fe131f24d037a833f92f566cc065b26";
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=imperial&appid=" + key;
 
@@ -77,14 +86,13 @@ $(submit).on("click", function (event) {
         // current day weather forecast
         $(todayTemp).text("Temp: " + response.main.temp);
 
-        $(todayIcon).attr("src","http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png" )
+        $(todayIcon).attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png")
 
+        $(todayHumid).text("Humidity: " + response.main.humidity + " %");
 
-        $(todayHumid).text("Humidity: " + response.main.humidity);
+        $(todayWind).text("Wind speed: " + response.wind.speed + " mph");
 
-        $(todayWind).text("Wind speed: " + response.wind.speed);
-
-        $(todayCloud).text("Clouds: " + response.clouds.all);
+        $(todayCloud).text("Clouds: " + response.clouds.all + " %");
 
         //   $(todayIndex).text("UV Index: " + response.main.temp)
 
@@ -98,57 +106,56 @@ $(submit).on("click", function (event) {
     }).then(function (result) {
         console.log(result);
 
-       
-          var  dayOne = result.list[4];
-          var  dayTwo = result.list[11];
-          var  dayThree = result.list[19];
-          var  dayFour = result.list[27];
-          var  dayFive = result.list[35];
-        //   var forecastIcon = result.list[i].weather[0].icon;
-// var resultList = [result.list[4],result.list[11],result.list[19],result.list[27],result.list[35]];
-//         for (var i = 0; i < resultList.length; i++) {
-            
-//                 $("#img-1").attr("src", result.list[i].weather[0].icon);
-           
-//         };
+
+        var dayOne = result.list[4];
+        var dayTwo = result.list[11];
+        var dayThree = result.list[19];
+        var dayFour = result.list[27];
+        var dayFive = result.list[35];
 
 
 
         // setting forecast info to proper html spots
+
+        //day One
         $("#temp-1").text(foreCast[0].temp + dayOne.main.temp);
-        $("#img-1").attr("src","http://openweathermap.org/img/wn/" + dayOne.weather[0].icon + "@2x.png" )
-        $("#humidity-1").text(foreCast[0].humidity + dayOne.main.humidity);
-        $("#wind-1").text(foreCast[0].wind + dayOne.wind.speed);
+        $("#img-1").attr("src", "http://openweathermap.org/img/wn/" + dayOne.weather[0].icon + "@2x.png")
+        $("#humidity-1").text(foreCast[0].humidity + dayOne.main.humidity + " %");
+        $("#wind-1").text(foreCast[0].wind + dayOne.wind.speed + " mph");
         // $("#index-1").text(foreCast[0].index + dayOne.main.temp);
-        $("#cloud-1").text(foreCast[0].cloud + dayOne.clouds.all);
+        $("#cloud-1").text(foreCast[0].cloud + dayOne.clouds.all + " %");
 
+        // day Two
         $("#temp-2").text(foreCast[0].temp + dayTwo.main.temp);
-        $("#img-2").attr("src","http://openweathermap.org/img/wn/" + dayTwo.weather[0].icon + "@2x.png" )
-        $("#humidity-2").text(foreCast[0].humidity + dayTwo.main.humidity);
-        $("#wind-2").text(foreCast[0].wind + dayTwo.wind.speed);
+        $("#img-2").attr("src", "http://openweathermap.org/img/wn/" + dayTwo.weather[0].icon + "@2x.png")
+        $("#humidity-2").text(foreCast[0].humidity + dayTwo.main.humidity + " %");
+        $("#wind-2").text(foreCast[0].wind + dayTwo.wind.speed + " mph");
         // $("#index-2").text(foreCast[0].index + result.list[4].main.temp);
-        $("#cloud-2").text(foreCast[0].cloud + dayTwo.clouds.all);
+        $("#cloud-2").text(foreCast[0].cloud + dayTwo.clouds.all + " %");
 
+        // day Three
         $("#temp-3").text(foreCast[0].temp + dayThree.main.temp);
-        $("#img-3").attr("src","http://openweathermap.org/img/wn/" + dayThree.weather[0].icon + "@2x.png" )
-        $("#humidity-3").text(foreCast[0].humidity + dayThree.main.humidity);
-        $("#wind-3").text(foreCast[0].wind + dayThree.wind.speed);
+        $("#img-3").attr("src", "http://openweathermap.org/img/wn/" + dayThree.weather[0].icon + "@2x.png")
+        $("#humidity-3").text(foreCast[0].humidity + dayThree.main.humidity + " %");
+        $("#wind-3").text(foreCast[0].wind + dayThree.wind.speed + " mph");
         // $("#index-3").text(foreCast[0].index + dayThree.main.temp);
-        $("#cloud-3").text(foreCast[0].cloud + dayThree.clouds.all);
+        $("#cloud-3").text(foreCast[0].cloud + dayThree.clouds.all + " %");
 
+        // day Four
         $("#temp-4").text(foreCast[0].temp + dayFour.main.temp);
-        $("#img-4").attr("src","http://openweathermap.org/img/wn/" + dayFour.weather[0].icon + "@2x.png" )
-        $("#humidity-4").text(foreCast[0].humidity + dayFour.main.humidity);
-        $("#wind-4").text(foreCast[0].wind + dayFour.wind.speed);
+        $("#img-4").attr("src", "http://openweathermap.org/img/wn/" + dayFour.weather[0].icon + "@2x.png")
+        $("#humidity-4").text(foreCast[0].humidity + dayFour.main.humidity + " %");
+        $("#wind-4").text(foreCast[0].wind + dayFour.wind.speed + " mph");
         // $("#index-4").text(foreCast[0].index + dayFour.main.temp);
-        $("#cloud-4").text(foreCast[0].cloud + dayFour.clouds.all);
+        $("#cloud-4").text(foreCast[0].cloud + dayFour.clouds.all + " %");
 
+        // day Five
         $("#temp-5").text(foreCast[0].temp + dayFive.main.temp);
-        $("#img-5").attr("src","http://openweathermap.org/img/wn/" + dayFive.weather[0].icon + "@2x.png" )
-        $("#humidity-5").text(foreCast[0].humidity + dayFive.main.humidity);
-        $("#wind-5").text(foreCast[0].wind + dayFive.wind.speed);
+        $("#img-5").attr("src", "http://openweathermap.org/img/wn/" + dayFive.weather[0].icon + "@2x.png")
+        $("#humidity-5").text(foreCast[0].humidity + dayFive.main.humidity + " %");
+        $("#wind-5").text(foreCast[0].wind + dayFive.wind.speed + " mph");
         // $("#index-5").text(foreCast[0].index + dayFive.main.temp);
-        $("#cloud-5").text(foreCast[0].cloud + dayFive.clouds.all);
+        $("#cloud-5").text(foreCast[0].cloud + dayFive.clouds.all + " %");
 
 
 
